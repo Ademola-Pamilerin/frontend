19c65f0fef1c3b6d971020f3d06ee7371a9fbd7a
Ademola-Pamilerin <adeakanfea@gmail.com>
2023-07-02 00:21:01 +0100

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          your Edit <code>src/App.jsx</code> and save to test HMR
+          your Edit <code>App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
